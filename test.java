8078c20aa4bfc0aed9244f75750aeccd3e0aed7e
techworldthink <jobin25051999@gmail.com>
2022-11-06 17:38:18 +0530

    JAVA  - Collection - generics

--- test.java
+++ test.java
@@ -1,41 +1,21 @@
-class LinearTimeComplexity {
 
-        static int[] bubbleSort(int[] arr) {
-                int n = arr.length;
-                int temp = 0;
-                for (int i = 0; i < n; i++) {
-                        for (int j = 1; j < (n - i); j++) {
-                                if (arr[j - 1] > arr[j]) {
-                                        // swap elements
-                                        temp = arr[j - 1];
-                                        arr[j - 1] = arr[j];
-                                        arr[j] = temp;
-                                }
+class Container<T> {
+        T data;
 
-                        }
-                }
-
-                return arr;
+        void showType() {
+                System.out.println(data.getClass().getName());
         }
+}
 
-        static void printArray(int[] arr) {
-                for (int i = 0; i < arr.length; i++)
-                        System.out.print(arr[i] + "\t");
-        }
+class Problem {
 
         public static void main(String[] args) {
+                Container<Integer> obj_1 = new Container<>();
+                obj_1.data = 1;
+                obj_1.showType();
 
-                int[] numbers = new int[10];
-
-                for (int i = 0; i < numbers.length; i++)
-                        numbers[i] = i;
-
-                numbers[0] = 123;
-
-                long start = System.currentTimeMillis();
-                numbers = bubbleSort(numbers);
-                System.out.println("Time taken : " + (System.currentTimeMillis() - start));
-                printArray(numbers);
-
+                Container<Double> obj_2 = new Container<>();
+                obj_2.data = 1.1;
+                obj_2.showType();
         }
 }
